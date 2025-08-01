20f5af7b863c399505e6209704b064e6db14b488
Edgardorc8 <133413493+Edgardorc8@users.noreply.github.com>
2025-07-31 20:34:11 -0700

    Delete Scripts directory

--- Scripts/insert-tipo_asesoria.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `tipo_asesoria` (`id_tipo_asesoria`, `nombre_tipo`) VALUES
-(1, 'Materia Civil'),
-(2, 'Materia LOPNA'),
-(3, 'Constitución'),
-(4, 'Materia Penal'),
-(5, 'Materia Ley de Tierra y desarrollo Agrario');
