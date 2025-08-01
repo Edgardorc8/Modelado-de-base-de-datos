7b19fbe7ce32c12cea0588e5ae9ca646ac58d0c2
Edgardorc8 <133413493+Edgardorc8@users.noreply.github.com>
2025-07-31 20:32:35 -0700

    Add files via upload

---
+++ Scripts/insert-tipo_asesoria.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tipo_asesoria` (`id_tipo_asesoria`, `nombre_tipo`) VALUES
+(1, 'Materia Civil'),
+(2, 'Materia LOPNA'),
+(3, 'Constitución'),
+(4, 'Materia Penal'),
+(5, 'Materia Ley de Tierra y desarrollo Agrario');
